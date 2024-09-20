DescentNetwork/Descent3

Move Base_directory into the cfile module
The main motivation behind this commit is to make it easier to create a
future commit. That future commit will will take multiple different
functions from throughout the codebase and replace them with a new
function named cf_LocatePath().

One of the functions that will get replaced is
cf_FindRealFileNameCaseInsensitive(). There are tests for
cf_FindRealFileNameCaseInsensitive() in cfile/tests/cfile_tests.cpp.
When I make that future commit, I will have to change the tests in
cfile/test/cfile_tests.cpp so that they test the cf_LocatePath()
function instead of the cf_FindRealFileNameCaseInsensitive() function.

There is an important difference between cf_LocatePath() and
cf_FindRealFileNameCaseInsensitive(). cf_LocatePath() depends on the
Base_directory variable. cf_FindRealFileNameCaseInsensitive() does not.
In order to update the tests so that they use cf_LocatePath(), I need to
make sure that the tests have access to the Base_directory variable.

Before this change, the Base_directory variable was declared in
Descent3/init.cpp. That meant that if a program wanted to access
Base_directory, then it would have to link to Descent3/init.cpp. In
other words, we would have to link to Descent3/init.cpp when compiling
the program that currently tests cf_FindRealFileNameCaseInsensitive()
but will test cf_LocatePath() in the future. I tried making that program
link to Descent3/init.cpp, but I gave up after a wile. Descent3/init.cpp
depends on a lot of other things in the codebase.

In order to make it easier to create that future commit, this commit
moves the Base_directory variable into the cfile module. When I create
that future commit, I won’t have to mess with anything linking related
because the cfile tests already link to the cfile module. Additionally,
this change will make compiling that test program more efficient.
There’s not need for the compiler to look at the entirety of
Descent3/init.cpp just because we need a single va

Descent3/init.cpp
@@ -1026,9 +1026,6 @@ static bool Graphics_init = false;
 static bool Title_bitmap_init = false;
 uint8_t Use_motion_blur = 0;
 
-// The "root" directory of the D3 file tree
-std::filesystem::path Base_directory;
-
 extern int Min_allowed_frametime;
 
 extern bool Render_powerup_sparkles;
@@ -1396,8 +1393,9 @@ void InitIOSystems(bool editor) {
   // Set the base directory
   int dirarg = FindArg("-setdir");
   int exedirarg = FindArg("-useexedir");
+  std::filesystem::path initial_base_directory;
   if (dirarg) {
-    Base_directory = GameArgs[dirarg + 1];
+    initial_base_directory = GameArgs[dirarg + 1];
   } else if (exedirarg) {
     char exec_path[_MAX_PATH];
     memset(exec_path, 0, sizeof(exec_path));
@@ -1406,13 +1404,14 @@ void InitIOSystems(bool editor) {
       Error("Failed to get executable path\n");
     } else {
       std::filesystem::path executablePath(exec_path);
-      Base_directory = executablePath.parent_path();
+      initial_base_directory = executablePath.parent_path();
       LOG_INFO << "Using working directory of " << Base_directory;
     }
   } else {
-    Base_directory = std::filesystem::current_path();
+    initial_base_directory = std::filesystem::current_path();
   }
 
+  cf_SetBaseDirectory(initial_base_directory);
   ddio_SetWorkingDir(Base_directory.u8string().c_str());
 
   Descent->set_defer_handler(D3DeferHandler);

Jayman2000/Descent3-pr

Descent3/init.cpp
@@ -1026,9 +1026,6 @@ static bool Graphics_init = false;
 static bool Title_bitmap_init = false;
 uint8_t Use_motion_blur = 0;
 
-// The "root" directory of the D3 file tree
-std::filesystem::path Base_directory;
-
 extern int Min_allowed_frametime;
 
 extern bool Render_powerup_sparkles;
@@ -1395,8 +1392,9 @@ void InitIOSystems(bool editor) {
   // Set the base directory
   int dirarg = FindArg("-setdir");
   int exedirarg = FindArg("-useexedir");
+  std::filesystem::path initial_base_directory;
   if (dirarg) {
-    Base_directory = GameArgs[dirarg + 1];
+    initial_base_directory = GameArgs[dirarg + 1];
   } else if (exedirarg) {
     char exec_path[_MAX_PATH];
     memset(exec_path, 0, sizeof(exec_path));
@@ -1405,13 +1403,14 @@ void InitIOSystems(bool editor) {
       Error("Failed to get executable path\n");
     } else {
       std::filesystem::path executablePath(exec_path);
-      Base_directory = executablePath.parent_path();
+      initial_base_directory = executablePath.parent_path();
       LOG_INFO << "Using working directory of " << Base_directory;
     }
   } else {
-    Base_directory = std::filesystem::current_path();
+    initial_base_directory = std::filesystem::current_path();
   }
 
+  cf_Init(initial_base_directory);
   ddio_SetWorkingDir(Base_directory.u8string().c_str());
 
   Descent->set_defer_handler(D3DeferHandler);